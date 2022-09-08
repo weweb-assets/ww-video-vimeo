91a7d03332c1c93d6936a48e4fda8c8bb3c33828
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-09-08 14:44:22 +0200

    Test build

--- ww-config.js
+++ ww-config.js
@@ -31,7 +31,7 @@ export default {
                 en: 'Start time (s)',
             },
             type: 'Number',
-            options: (_, sidepanelContent) => ({ min: 0, max: sidepanelContent.videoDuration }),
+            options: (_, sidepanelContent) => ({ min: 0, max: 30 }),
             section: 'settings',
             bindable: true,
             defaultValue: 0,
